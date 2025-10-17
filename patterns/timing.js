1fdc3a0f132127af885faf24d88d2091cbd4785d
Mackenzie Green <macgreen813@outlook.com>
2025-10-16 19:30:06 -0700

    timing tests passed

--- patterns/timing.js
+++ patterns/timing.js
@@ -19,7 +19,8 @@ export function isSongInPlaylist(playlist, song) {
  * @returns `undefined` if `numbers` is empty
  */
 export function isAllEven(numbers) {
-  // TODO
+  if (numbers.length === 0) return undefined;
+  return numbers.every((num) => num % 2 === 0);
 }
 
 /**
@@ -32,5 +33,13 @@ export function isAllEven(numbers) {
  * @returns {boolean} whether there is enough food in the backpack to feed everyone
  */
 export function haveEnoughFood(backpack, people) {
-  // TODO
+  let foodCount = 0;
+
+  for (let i = 0; i < backpack.length; i++) {
+    if (backpack[i].category === "food") {
+      foodCount++;
+    }
+  }
+
+  return foodCount >= people;
 }
